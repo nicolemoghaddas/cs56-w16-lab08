6e7c8b03a79719ea3bcfbef48d2e5c523545596d
Nicole Moghaddas <nmoghaddas@umail.ucsb.edu>
2016-02-22 20:08:24 -0800

    added ENERGY var

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,7 +27,7 @@ public class Main {
 
     get("/hello", (req, res) -> {
 	RelativisticModel.select();
-	String energy = System.getenv().get("Energy");
+	String energy = System.getenv().get("ENERGY");
 	Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
 	return "E=mc^2: " + energy + " = " + m.toString();
     });
